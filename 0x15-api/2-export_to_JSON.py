b4f9939cd009e665659334a490781d28b81b62b4
Makuwe <mmakuwe98@gmail.com>
2024-08-08 07:00:57 -0700

    API Tasks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,12 +7,10 @@ import sys
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
-    
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
     
     params = {"userId": user_id}
-    
     todos = requests.get(url + "todos", params=params).json()
     data_to_export = {user_id: []}
     
@@ -21,7 +19,7 @@ if __name__ == '__main__':
             "task": todo.get("title"),
             "completed": todo.get("completed"),
             "username": username
-        }
+            }
         data_to_export[user_id].append(task_info)
 
     with open("{}.json".format(user_id), "w") as jsonfile:
