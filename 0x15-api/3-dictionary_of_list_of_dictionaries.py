130e8213ffff970653fb71984b4867410c1842e0
Makuwe <mmakuwe98@gmail.com>
2024-08-08 07:07:21 -0700

    task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,11 +5,12 @@ import json
 import requests
 import sys
 
+
 def fetch_user_data():
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
     data_to_export = {}
-    
+
     for user in users:
         user_id = user["id"]
         todo_response = requests.get(url + f"todos?userId={user_id}")
@@ -25,6 +26,7 @@ def fetch_user_data():
             data_to_export[user_id].append(task_info)
 
     return data_to_export
+
 
 if __name__ == '__main__':
     data_to_export = fetch_user_data()
